5fb04ec330129702b82d2ffa3a2eb6a4399fa63b
coderpotter <anighojkar@usf.edu>
2021-04-29 13:11:29 -0400

    converted input array of numbers to strings

---
+++ main.js
@@ -0,0 +1,15 @@
+var dict = {
+    '1':'One',
+    '2':'Two',
+    '3':'Three',
+    '4': 'Four',
+    '5':'Five',
+    '6':'Six',
+    '7':'Seven',
+    '8':'Eight',
+    '9':'Nine',
+    '0':'Zero'
+};
+
+var inputs = process.argv.slice(2).map(function(e){return e.toString()});
+console.log(inputs)
